f52af05f74e3c32df5daf1ca7650b3ea690e9801
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-08-16 08:30:24 +0200

    modal

---
+++ index.js
@@ -0,0 +1,19 @@
+const modalForm = document.getElementById("contact-modal");
+const modalClose = document.getElementById("exampleModal");
+
+const sendModalMessage = (e) => {
+  e.preventDefault();
+  //   get the form values
+  const userMessage = document.getElementById("message");
+  const userMail = document.getElementById("email-address");
+
+  const message = userMessage.value;
+  const mail = userMail.value;
+
+  //   Send the user's email and message to the API for email delivery.
+
+  //   Reset form inputs
+  e.target.reset();
+};
+
+modalForm.addEventListener("submit", sendModalMessage);
