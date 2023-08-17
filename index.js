0cbf60f0b346e133644ade5a5d95514444765315
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-08-17 08:39:42 +0200

    SimpleLightbox

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+// Gallery
+const moreBtn = document.getElementById("loadMore");
+const galleryBackdrop = document.getElementById("backdrop");
+const gallery = document.getElementById("grid-backdrop");
+// Modal
+const modalForm = document.getElementById("contact-modal");
+const modalClose = document.getElementById("exampleModal");
+
 // set up simple lightbox gallery
-const lightbox = new SimpleLightbox(".grid a", {
+
+let lightbox = new SimpleLightbox(".grid a", {
   captionsData: "alt",
   captionPosition: "bottom",
 });
 
-window.onload = () => {
-  const elem = document.querySelector(".grid");
-  const msnry = new Masonry(elem, {
-    // options
-    itemSelector: ".grid-item",
-    gutter: 42,
-    fitWidth: true,
+// Masonry initialization
+const masonryLoadPhotos = () => {
+  imagesLoaded(gallery, function (instance) {
+    const elem = document.querySelector(".grid");
+    const msnry = new Masonry(elem, {
+      itemSelector: ".grid-item",
+      gutter: 42,
+      fitWidth: true,
+    });
   });
 };
 
+masonryLoadPhotos();
+
 // modal section
-const modalForm = document.getElementById("contact-modal");
-const modalClose = document.getElementById("exampleModal");
 
 const sendModalMessage = (e) => {
   e.preventDefault();
@@ -37,17 +48,89 @@ modalForm.addEventListener("submit", sendModalMessage);
 
 // Project load more
 
-const moreBtn = document.getElementById("loadMore");
+const imageObjects = [
+  {
+    src: "utils/jungle_stairs.png",
+    big: "utils/bigger_images/jungle_stairs_xxl.png",
+    alt: "Jungle Stairs",
+  },
+  {
+    src: "utils/pool_garden.png",
+    big: "utils/bigger_images/pool_garden_xxl.png",
+    alt: "Pool Garden",
+  },
+  {
+    src: "utils/rose_path.png",
+    big: "utils/bigger_images/rose_path_xxl.png",
+    alt: "Rose Path",
+  },
+  {
+    src: "utils/mosaic_gazebo.png",
+    big: "utils/bigger_images/mosaic_gazebo_xxl.png",
+    alt: "Mosaic Gazebo",
+  },
+  {
+    src: "utils/garden_path.png",
+    big: "utils/bigger_images/garden_path_xxl.png",
+    alt: "Garden Path",
+  },
+  {
+    src: "utils/fish.png",
+    big: "utils/bigger_images/fish_xxl.png",
+    alt: "Fish",
+  },
+  {
+    src: "utils/big_gazebo.png",
+    big: "utils/bigger_images/big_gazebo_xxl.png",
+    alt: "Big Gazebo",
+  },
+  {
+    src: "utils/stairs.png",
+    big: "utils/bigger_images/stairs_xxl.png",
+    alt: "Stairs",
+  },
+  {
+    src: "utils/garden_gazebo.png",
+    big: "utils/bigger_images/garden_gazebo_xxl.png",
+    alt: "Garden Gazebo",
+  },
+];
+
+// when moreBtn is clicked, add more photos
+// i would fetch more photos from DB but for this project it's easier to do it that way
+
+const createHtmlPhotos = () => {
+  let HTMLCode = imageObjects
+    .map((photoObject) => {
+      const { src, big, alt } = photoObject;
+
+      return `
+  <div class="grid-item">
+    <a href="${big}">
+      <img class="gallery-item" src="${src}" alt="${alt}">
+    </a>
+  </div>`;
+    })
+    .join("");
+  return HTMLCode;
+};
 
 const loadMorePhoto = () => {
   // get grid backdrop and remove backdrop
-  const sectionBackdrop = document.getElementById("grid-backdrop");
-  sectionBackdrop.classList.remove("grid-backdrop");
+
+  gallery.classList.remove("grid-backdrop");
 
   // hide button
   moreBtn.classList.add("hide");
 
   // load more photo
+  let morePhotos = createHtmlPhotos();
+  // add photos to DOM
+  gallery.innerHTML = gallery.innerHTML + morePhotos;
+  // trigger masonry and simpleLight to refresh
+  masonryLoadPhotos();
+  lightbox.refresh();
+  console.log(lightbox);
 };
 
 moreBtn.addEventListener("click", loadMorePhoto);
